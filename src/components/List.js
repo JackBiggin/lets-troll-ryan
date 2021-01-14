644f0df7111f37cdc3ca1c3019bcd847cb20334e
Jacklyn Biggin <jacklyn@jmb.im>
2021-01-13 22:05:41 -0500

    changes

--- src/components/List.js
+++ src/components/List.js
@@ -3,15 +3,30 @@ import "./List.css"
 import ListItem from "./ListItem";
 
 function List() {
-  const [trolls, setTrolls] = useState({});
+  const [trolls, setTrolls] = useState([
+    "Send Ryan a BLAHAJ",
+    "Heckle Ryan on stream when he concentrating",
+    "Pretend he doesn't exist when he needs something urgent"
+  ]);
+
+  
+
+  const listItems = trolls.map((troll) =>
+    <ListItem key={troll} item={troll} />
+  );
+
+  function addToTrolls(troll) {
+    const currentTrolls = [...trolls]
+    currentTrolls.push(troll)
+    setTrolls(currentTrolls)
+  }
 
   return (
     <div className="list-container">
       <h2 className="list-header">My Trolls for Ryan in 2021</h2>
       <input placeholder="Type a new troll here..." className="list-input"></input>
-      <ListItem item="Send Ryan a BLAHAJ" />
-      <ListItem item="Heckle Ryan on stream when he concentrating" />
-      <ListItem item="Pretend he doesn't exist when he needs something urgent" />
+      <button onClick={() => addToTrolls("test")}>Add test troll</button>
+      {listItems}
     </div>
   );
 }
