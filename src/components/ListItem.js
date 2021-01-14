6f9a20166d446440bb9e4f32764a259f7c08e95b
Jacklyn Biggin <jacklyn@jmb.im>
2021-01-13 22:27:56 -0500

    adam asked for this

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,11 +1,20 @@
+import React, { useState } from 'react';
 import { Col, Row, Form } from "react-bootstrap"
 import "./ListItem.css"
 
 function ListItem(props) {
+    const [isComplete, setIsComplete] = useState(false)
+
   return (
     <div className="list-item">
         <Row>
-            <Col xs={1}><Form.Check type="checkbox" /></Col>
+            <Col xs={1}>
+                <Form.Check
+                    type="checkbox"
+                    value={isComplete}
+                    onChange={ () => {setIsComplete(!isComplete)} }
+                    />
+            </Col>
             <Col xs={11}>{props.item}</Col>
         </Row>
     </div>
